9eee2922e49b77e8aaa8d38c52d742ae33feb7cf
Suryathangaraj <suryathangam765@gmail.com>
2026-01-02 00:52:28 +0800

    get particular id details from another branch name is getOrderById

--- src/main/java/com/example/learnGit/Controller/HomeController.java
+++ src/main/java/com/example/learnGit/Controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.learnGit.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +17,23 @@ public class HomeController {
 		return "Order Placed Successfull";
 		
 	}
-	
+	@GetMapping(value="/getOrderDetails")
 	public ProductDetailsPOJO getDetails() {
+		
+		ProductDetailsPOJO productDetailsPOJO = new ProductDetailsPOJO();
+		
+		productDetailsPOJO.setProdId("0001");
+		productDetailsPOJO.setProdName("Dove");
+		productDetailsPOJO.setProdCatagery("Soap");
+		productDetailsPOJO.setProdDescription("for Smooth Skin");
+		productDetailsPOJO.setOrderbyNo(1);
+		
+		return productDetailsPOJO;
+		
+	}
+	
+	@GetMapping(value = "/getOrderById",params = "id")
+ 	public ProductDetailsPOJO getOrderDetailsbyId(String id) {
 		
 		ProductDetailsPOJO productDetailsPOJO = new ProductDetailsPOJO();
 		

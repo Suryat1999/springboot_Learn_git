57b9dffb4a79edc1481eb14f12d75edc8fec8b78
Suryathangaraj <suryathangam765@gmail.com>
2026-01-03 00:27:21 +0800

    Revert "adding client requirement to get details from service layer"
    This reverts commit cc38b3c1137b3c2064cfa84996b38c4a100fcc1a.

--- src/main/java/com/example/learnGit/Controller/HomeController.java
+++ src/main/java/com/example/learnGit/Controller/HomeController.java
@@ -1,18 +1,13 @@
 package com.example.learnGit.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.learnGit.Service.OrderService;
 import com.example.learnGit.model.ProductDetailsPOJO;
 
 @RestController
 public class HomeController {
 
-	@Autowired
-	public OrderService orderService;
 	
 	@PostMapping(value = "/insert")
 	public String insertOrderDetails(ProductDetailsPOJO productDetailsPOJO) {
@@ -21,29 +16,5 @@ public class HomeController {
 		return "Order Placed Successfull";
 		
 	}
-	@GetMapping(value="/getOrderDetails")
-	public ProductDetailsPOJO getDetails() {
-		
-		ProductDetailsPOJO productDetailsPOJO = new ProductDetailsPOJO();
-		
-		productDetailsPOJO.setProdId("0001");
-		productDetailsPOJO.setProdName("Dove");
-		productDetailsPOJO.setProdCatagery("Soap");
-		productDetailsPOJO.setProdDescription("for Smooth Skin");
-		productDetailsPOJO.setOrderbyNo(1);
-		
-		return productDetailsPOJO;
-		
-	}
-	
-	@GetMapping(value = "/getOrderById",params = "id")
- 	public ProductDetailsPOJO getOrderDetailsbyId(String id) {
-		
-		ProductDetailsPOJO productDetailsPOJO = orderService.getOrderDetailsbyId(id);
-
-		return productDetailsPOJO;
-		
-	}
-	
 	
 }
